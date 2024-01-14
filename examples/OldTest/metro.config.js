6026c52a06c325fc83476805cd861cb3096efbfc
iyegoroff <iegoroff@gmail.com>
2024-01-14 03:29:36 +0300

    upd OldTest to rn 0.73.2

--- examples/OldTest/metro.config.js
+++ examples/OldTest/metro.config.js
@@ -1,37 +1,29 @@
-const path = require('path');
-const exclusionList = require('metro-config/src/defaults/exclusionList');
-const escape = require('escape-string-regexp');
-const pak = require('../../package.json');
+const path = require('path')
+const { getDefaultConfig, mergeConfig } = require('@react-native/metro-config')
+const exclusionList = require('metro-config/src/defaults/exclusionList')
+const escape = require('escape-string-regexp')
+const pak = require('../../package.json')
 
-const root = path.resolve(__dirname, '../..');
+const root = path.resolve(__dirname, '../..')
 
 const modules = Object.keys({
-  ...pak.peerDependencies,
-});
+  ...pak.peerDependencies
+})
 
-module.exports = {
+const config = {
   projectRoot: __dirname,
   watchFolders: [root],
 
   resolver: {
     blockList: exclusionList(
-      modules.map(
-        m => new RegExp(`^${escape(path.join(root, 'node_modules', m))}\\/.*$`),
-      ),
+      modules.map((m) => new RegExp(`^${escape(path.join(root, 'node_modules', m))}\\/.*$`))
     ),
 
     extraNodeModules: modules.reduce((acc, name) => {
-      acc[name] = path.join(__dirname, 'node_modules', name);
-      return acc;
-    }, {}),
-  },
+      acc[name] = path.join(__dirname, 'node_modules', name)
+      return acc
+    }, {})
+  }
+}
 
-  transformer: {
-    getTransformOptions: async () => ({
-      transform: {
-        experimentalImportSupport: false,
-        inlineRequires: true,
-      },
-    }),
-  },
-};
+module.exports = mergeConfig(getDefaultConfig(__dirname), config)
